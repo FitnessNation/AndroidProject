1096da6e13aedb329ecf45412085f0a9996a7fd5
Cozma Daniel <cozma.daniel99@gmail.com>
2020-06-09 20:19:18 +0300

    Commit3: Log in activity added + menu_login_fragment added

--- FitnessNation/app/src/main/java/com/example/fitnessnation/activitites/LoginActivity.kt
+++ FitnessNation/app/src/main/java/com/example/fitnessnation/activitites/LoginActivity.kt
@@ -3,11 +3,26 @@ package com.example.fitnessnation.activitites
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.fitnessnation.R
+import com.example.fitnessnation.fragments.MenuLoginFragment
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        setFirstFragment()
+    }
+
+    fun setFirstFragment() {
+        val fragment: MenuLoginFragment = MenuLoginFragment.newInstance()
+
+        val transaction =supportFragmentManager.beginTransaction()
+      transaction.add(R.id.primary_frame_s_session, fragment)
+        transaction.addToBackStack(MenuLoginFragment.toString())
+        transaction.commit()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
