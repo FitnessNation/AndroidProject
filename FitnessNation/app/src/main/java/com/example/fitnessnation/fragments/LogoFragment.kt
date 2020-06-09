206788d64a1023c67784a42b50301c32473d2b5c
Szilvia <szilviakovacs55@gmail.com>
2020-06-09 19:50:46 +0300

    Commit 2: login activity added, with start button functioanility

--- FitnessNation/app/src/main/java/com/example/fitnessnation/fragments/LogoFragment.kt
+++ FitnessNation/app/src/main/java/com/example/fitnessnation/fragments/LogoFragment.kt
@@ -1,5 +1,6 @@
 package com.example.fitnessnation.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.fitnessnation.R
+import com.example.fitnessnation.activitites.LoginActivity
 import kotlinx.android.synthetic.main.fragment_logo.*
 
 class LogoFragment : Fragment() {
@@ -14,29 +16,20 @@ class LogoFragment : Fragment() {
         fun newInstance() = LogoFragment()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_logo, container, false)
-    }
+    override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?,  savedInstanceState: Bundle?)
+            = inflater.inflate(R.layout.fragment_logo, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val toast = Toast.makeText(this.context, "This is F1A2", Toast.LENGTH_SHORT).show()
+
         btn_start.setOnClickListener {
-            //toTheSecondFragment()
-            val toast = Toast.makeText(this.context, "This is F2A2", Toast.LENGTH_SHORT).show()
+            goToNextActivity()
         }
-
-
+    }
+    fun goToNextActivity()
+    {
+        val intent = Intent(activity, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
-/*fun toTheSecondFragment()
-{
-    val transaction = fragmentManager!!.beginTransaction()
-    transaction.replace(R.id.primary_frame_s_session,ThirdFragment.newInstance())
-    transaction.addToBackStack(ThirdFragment.toString())
-    transaction.commit()
-}*/
+
