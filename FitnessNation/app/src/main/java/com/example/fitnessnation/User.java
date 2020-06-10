3b49b8a1811b7edb978e73657896c4da174f1403
Szilvia <szilviakovacs55@gmail.com>
2020-06-10 14:33:45 +0300

    commit 6(Bianca): User entity created

---
+++ FitnessNation/app/src/main/java/com/example/fitnessnation/User.java
@@ -0,0 +1,37 @@
+package com.example.fitnessnation;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "users")
+public class User {
+
+
+    @PrimaryKey
+    @NonNull
+
+    @ColumnInfo(name = "user_username")
+    private String username;
+
+
+    @ColumnInfo(name = "user_password")
+    private String password;
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
