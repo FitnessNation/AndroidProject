361a781125d54065f01234c9f81fb11abf6fb03f
Szilvia <szilviakovacs55@gmail.com>
2020-06-17 23:54:10 +0300

    Revert "COMMIT 16: RecyclerViewAdapter created, it shows 3 columns of foods, click on food listener added so you can view the description about the food, layout created for the foods"
    This reverts commit 15a4576f5fb075d014c076e984d05edebd0d72b6.

--- FitnessNation/app/src/main/java/com/example/fitnessnation/Recycler.java
+++ FitnessNation/app/src/main/java/com/example/fitnessnation/Recycler.java
@@ -1,25 +1,15 @@
 package com.example.fitnessnation;
 
 
-import android.content.Context;
-import android.content.Intent;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
-import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.example.fitnessnation.activitites.FoodActivity;
-
 import java.util.List;
 
 public class Recycler extends RecyclerView.Adapter<Recycler.MyViewHolder> {
 
-     private Context context;
     private List<Meal> mealList;
 
     public Recycler(List<Meal> mealList)
@@ -31,33 +21,14 @@ public class Recycler extends RecyclerView.Adapter<Recycler.MyViewHolder> {
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-        View view;
-        view= LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_item_meal,parent,false);
-
-        return new MyViewHolder(view);
+        TextView textView = (TextView) LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_item_meal,parent,false);
+        MyViewHolder myViewHolder = new MyViewHolder(textView);
+        return myViewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        holder.food_name.setText(mealList.get(position).getName());
-        holder.food_image.setImageResource(mealList.get(position).getImagePath());
-        holder.cardView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-             Intent intent=new Intent(context, FoodActivity.class);
-             intent.putExtra("FoodName",mealList.get(position).getName());
-             intent.putExtra("FoodType",mealList.get(position).getType());
-             intent.putExtra("FoodDescription",mealList.get(position).getDescription());
-             intent.putExtra("FoodPic",mealList.get(position).getImagePath());
-             context.startActivity(intent);
-
-
-            }
-        }) ;
-
-
-
-
+        holder.items.setText((CharSequence) mealList.get(position));
     }
 
     @Override
@@ -67,16 +38,10 @@ public class Recycler extends RecyclerView.Adapter<Recycler.MyViewHolder> {
 
     public static  class MyViewHolder extends RecyclerView.ViewHolder {
 
-        TextView food_name;
-        ImageView food_image;
-        CardView cardView;
-
-        public MyViewHolder(@NonNull View itemView) {
+        TextView items;
+        public MyViewHolder(@NonNull TextView itemView) {
             super(itemView);
-
-            food_name=(TextView) itemView.findViewById(R.id.nameFood);
-            food_image=(ImageView) itemView.findViewById(R.id.imageFood);
-            cardView=(CardView)itemView.findViewById(R.id.cardview);
+            items = itemView;
         }
     }
 }
